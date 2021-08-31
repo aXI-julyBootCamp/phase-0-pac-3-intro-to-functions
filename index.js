9f5cc2cb7aaf8b1913b7cc76d2726715ba9b9451
JJ <jc@orbsdigital.com>
2021-08-31 16:46:54 +1000

    lab practice

--- index.js
+++ index.js
@@ -44,9 +44,30 @@ function add(x, y) {
     return x + y;//When we return inside a function, we're giving that value back to the world outside the function.
 }
 console.log(add(1,2));
-*/
 
 function say(greeting, firstName) {
     return `${greeting}, ${firstName}!`
 }
 console.log(say("Hello", "Sofia"));
+
+//return x + y;Setting up your function to "return" a value means that value is available to be used elsewhere in your program.
+
+// If we wanted to both return and log a string in our say() function, we might try writing:
+//
+function say(greeting, firstName) {
+    console.log('I was called!');
+    return `${greeting}, ${firstName}!`;//return ends the execution inside the function.
+}
+console.log(say("Howdy", "partner"));
+console.log("Hey hey!",say("Howdy", "partner"));
+
+
+//practice
+const percentage = 100;
+
+function inPercentage (num1) {
+    return percentage * num1;
+}
+console.log("Hey that was a", inPercentage(2),"% return!");
+*/
+
